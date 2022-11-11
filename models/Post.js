a2e756932e59000ee2278233f32f622e0104c777
ParksFG <ParkerFGay@gmail.com>
2022-11-11 11:30:45 -0600

    Initial Post.js model

---
+++ models/Post.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequalize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        date: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            },
+        },
+    }, {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
