19495bc76dbe6bbcccd9ae5c1c9e42adccad42e5
ParksFG <ParkerFGay@gmail.com>
2022-11-14 13:23:32 -0600

    Changed seed data

--- seeds/postData.js
+++ seeds/postData.js
@@ -5,17 +5,17 @@ const postData =
         {
             "title": "The use of the windows key",
             "content": "You WON'T believe this, but the windows key pulls up your search bar!  How crazy is that?!",
-            "userID" : "1"
+            "userID" : "3"
         },
         {
             "title": "I LOVE the new Apple Macbook",
             "content": "Yeah I'm mr techluver99, but the new Macbook is amazing.  It has all the best features that I'm known to love!",
-            "userID": "2"
+            "userID": "1"
         },
         {
             "title": "I have given up on javascript",
             "content": "Javascript has driven me to insanity so I've decided to takeup cross-stitching.",
-            "userID": "3"
+            "userID": "2"
         }
     ]
 
